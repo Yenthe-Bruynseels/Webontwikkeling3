c9724395cad1b9a7fe962a202ce26ed6f5add21a
Yenthe-Bruynseels <55389668+Yenthe-Bruynseels@users.noreply.github.com>
2020-10-07 15:45:49 +0200

    doe ma

--- src/main/java/domain/db/PersonDbInMemory.java
+++ src/main/java/domain/db/PersonDbInMemory.java
@@ -8,56 +8,61 @@ import java.util.List;
 import java.util.Map;
 
 public class PersonDbInMemory {
-	private Map<String, Person> persons = new HashMap<>();
-	
-	public PersonDbInMemory() {
-		Person administrator = new Person("admin", "admin@ucll.be", "t", "Ad", "Ministrator");
-		Person yenthe = new Person("yenthe", "yenthe@thibault.islove", "t", "Yen", "The");
-		Person thibault = new Person("thibault", "thibault@single.forever", "z&g=/=nietokay", "Thi", "Bault");
-		add(administrator);
-		add(yenthe);
-		add(thibault);
-	}
-	
-	public Person get(String personId){
-		if(personId == null){
-			throw new DbException("No id given");
-		}
-		return persons.get(personId);
-	}
-	
-	public List<Person> getAll(){
-		return new ArrayList<Person>(persons.values());	
-	}
+    private Map<String, Person> persons = new HashMap<>();
 
-	public void add(Person person){
-		if(person == null){
-			throw new DbException("No person given");
-		}
-		if (persons.containsKey(person.getUserid())) {
-			throw new DbException("User already exists");
-		}
-		persons.put(person.getUserid(), person);
-	}
-	
-	public void update(Person person){
-		if(person == null){
-			throw new DbException("No person given");
-		}
-		if(!persons.containsKey(person.getUserid())){
-			throw new DbException("No person found");
-		}
-		persons.put(person.getUserid(), person);
-	}
-	
-	public void delete(String personId){
-		if(personId == null){
-			throw new DbException("No id given");
-		}
-		persons.remove(personId);
-	}
+    public PersonDbInMemory() {
+        Person administrator = new Person("admin", "admin@ucll.be", "t", "Ad", "Ministrator");
+        Person yenthe = new Person("yenthe", "yenthe@thibault.islove", "t", "Yen", "The");
+        Person thibault = new Person("thibault", "thibault@single.forever", "z&g=/=nietokay", "Thi", "Bault");
+        add(administrator);
+        add(yenthe);
+        add(thibault);
+    }
 
-	public int getNumberOfPersons() {
-		return persons.size();
-	}
+    public Person get(String personId) {
+        if (personId == null) {
+            throw new DbException("No id given");
+        }
+        return persons.get(personId);
+    }
+
+    public List<Person> getAll() {
+        return new ArrayList<Person>(persons.values());
+    }
+
+    public void add(Person person) {
+        if (person == null) {
+            throw new DbException("No person given");
+        }
+        if (persons.containsKey(person.getUserid())) {
+            throw new DbException("User already exists");
+        }
+        persons.put(person.getUserid(), person);
+    }
+
+    public void update(Person person) {
+        if (person == null) {
+            throw new DbException("No person given");
+        }
+        if (!persons.containsKey(person.getUserid())) {
+            throw new DbException("No person found");
+        }
+        persons.put(person.getUserid(), person);
+    }
+
+    public void delete(String personId) {
+        if (personId == null) {
+            throw new DbException("No id given");
+        }
+        persons.remove(personId);
+    }
+
+    public int getNumberOfPersons() {
+        return persons.size();
+    }
+
+    public Person getPersonIfAuthenticated(String name, String password) {
+
+        return null;
+    }
 }
